3a1876386da6b676104ee26a765c144876655da0
sinan <sinantk56@gmail.com>
2024-03-18 16:44:29 +0530

    Qrscannerstage2

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:qrscanner/reg1.dart';
+import 'dart:convert';
 
+
+import 'package:flutter/material.dart';
+import 'package:qrscanner/qrscan.dart';
+import 'package:qrscanner/reg1.dart';
+import 'package:http/http.dart' as http;
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,9 +14,31 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController rollno = TextEditingController();
+  TextEditingController password = TextEditingController();
+  Future<void> login() async {
+    Uri uri = Uri.parse('https://scnner-web.onrender.com/api/login');
+    var response = await http.post(uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+        body: jsonEncode({'rollno': rollno.text, 'password': password.text}));
+    print(response.statusCode);
+    print(response.body);
+    var data = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return Qrsc();
+      }));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed')));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.teal,
       body: Column(
         children: [
@@ -28,7 +54,7 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           const SizedBox(height: 60),
           TextField(
-
+            controller: rollno,
             decoration: const InputDecoration(
               labelText: 'Enter your ROLL NUMBER',
               labelStyle: TextStyle(color: Colors.white),
@@ -42,7 +68,7 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           const SizedBox(height: 30),
           TextField(
-
+            controller: password,
             decoration: const InputDecoration(
               labelText: 'Enter your PASSWORD',
               labelStyle: TextStyle(color: Colors.white),
@@ -57,7 +83,9 @@ class _LoginScreenState extends State<LoginScreen> {
           const SizedBox(height: 40),
           ElevatedButton(
             style: ElevatedButton.styleFrom(primary: Colors.teal),
-            onPressed: () {},
+            onPressed: () {
+             login();
+            },
             child: const Text(
               'Login',
               style: TextStyle(color: Colors.white),
@@ -74,7 +102,9 @@ class _LoginScreenState extends State<LoginScreen> {
           ElevatedButton(
             style: ElevatedButton.styleFrom(primary: Colors.teal),
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context){return Reg();}));
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return Reg();
+              }));
             },
             child: const Text(
               'REGISTER NOW!',
